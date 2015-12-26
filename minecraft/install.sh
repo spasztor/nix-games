64786da80df9b8eab23b7c384d38796b597bdc38
spasztor <blankityblankblankblank@gmail.com>
2015-12-26 10:38:34 -0800

    Updated installation file.

--- minecraft/install.sh
+++ minecraft/install.sh
@@ -7,14 +7,16 @@ MINECRAFT_VER="1.8.9"
 JAR_SOURCE="https://s3.amazonaws.com/Minecraft.Download/versions/${MINECRAFT_VER}/minecraft_server.${MINECRAFT_VER}.jar"
 MAIN_JAR="minecraft_server.jar"
 
-#if [id -u ${MC_USER} &>/dev/null]; then
+#if [id -u ${MC_USER} &>/dev/null]
+#then
     #groupadd ${MC_USER_GROUP}
     #adduser ${MC_USER} ${MC_GROUP}
     #echo ${MC_USER_PASSWORD} | passwd ${MC_USER} --stdin
     #sudo chown -R ${MC_USER}:${MC_GROUP} ${SERVER_ROOT}
 #fi
 
-if [! -d ${SERVER_ROOT}]; then
+if [! -d ${SERVER_ROOT}]
+then
     mkdir ${SERVER_ROOT}
     chown -R ${MC_USER}:${MC_USER_GROUP} ${SERVER_ROOT}
 fi
