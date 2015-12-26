879de4c662e56dcbef8aba13dcd9d93394b0c87e
spasztor <blankityblankblankblank@gmail.com>
2015-12-26 11:38:40 -0800

    Updated user configuration and testing dirname use.

--- minecraft/install.sh
+++ minecraft/install.sh
@@ -7,20 +7,26 @@ MINECRAFT_VER="1.8.9"
 JAR_SOURCE="https://s3.amazonaws.com/Minecraft.Download/versions/${MINECRAFT_VER}/minecraft_server.${MINECRAFT_VER}.jar"
 MAIN_JAR="minecraft_server.jar"
 
-#if [id -u ${MC_USER} &>/dev/null]
-#then
-    #groupadd ${MC_USER_GROUP}
-    #adduser ${MC_USER} ${MC_GROUP}
-    #echo ${MC_USER_PASSWORD} | passwd ${MC_USER} --stdin
-    #sudo chown -R ${MC_USER}:${MC_GROUP} ${SERVER_ROOT}
-#fi
-
-if [! -d ${SERVER_ROOT}]
+if [ -z "$(getent passwd ${MC_USER})" ]
 then
+    echo "${MC_USER} user not found. Adding..."
+    groupadd ${MC_USER_GROUP}
+    useradd -g ${MC_USER_GROUP} ${MC_USER}
+    echo ${MC_USER}:${MC_USER_PASSWORD} | chpasswd
+else
+    echo "${MC_USER} user found."
+fi
+
+if [ ! -d ${SERVER_ROOT} ]
+then
+    echo "${SERVER_ROOT} not found. Creating..."
     mkdir ${SERVER_ROOT}
     chown -R ${MC_USER}:${MC_USER_GROUP} ${SERVER_ROOT}
 fi
 
-wget --output-document=${SERVER_ROOT}/${MAIN_JAR} ${JAR_SOURCE}
-cp ./minecraft-server.sh ${SERVER_ROOT}/minecraft-server.sh
-cp ./minecraft-server.conf /etc/init/minecraft-server.conf
+echo "Downloading jar file..."
+#wget -O ${SERVER_ROOT}/${MAIN_JAR} ${JAR_SOURCE}
+echo "Installing scripts..."
+echo $(dirname "$BASH_SOURCE")
+#cp $(dirname "$BASH_SOURCE")./minecraft-server.sh ${SERVER_ROOT}/minecraft-server.sh
+#cp $(dirname "$BASH_SOURCE")./minecraft-server.conf /etc/init/minecraft-server.conf
